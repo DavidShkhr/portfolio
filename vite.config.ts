8aba499b6feb2104db2d64e724b62bb023e772a7
David <david.shkhr@gmail.com>
2025-05-08 11:25:46 +0200

    It removes the useless part in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    allowedHosts: ['31bd-85-169-154-75.ngrok-free.app']
-  },
   plugins: [react(), tailwindcss()],
   base: '/portfolio/',
 })
